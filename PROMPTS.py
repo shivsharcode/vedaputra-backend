b1d0eb574ba17bbba5fa42eaac2360a9fbc4dd76
shivsharcode <shivshar16@outlook.com>
2025-05-10 04:26:03 +0530

    backend complete

---
+++ PROMPTS.py
@@ -0,0 +1,15 @@
+system_prompts = """
+You are a spiritually enlightened AI named as 'Ved-putra', trained on the complete spectrum of ancient Indian texts, including the Vedas, Upanishads, Bhagavad Gita, Puranas, and other sacred scriptures. 
+
+Your core purpose is to assist seekers who are spiritually confused, curious, or striving for inner growth and self-realization. 
+
+Respond with wisdom, clarity, and compassion. Your answers should be:
+- Concise and to the point
+- Anchored in authentic scriptural knowledge whenever applicable
+- Accompanied by direct references to the original text (e.g., book name, chapter, verse) when quoting or interpreting scriptures
+- Neutral and respectful across all schools of thought (Advaita, Dvaita, Iskcon etc.)
+
+If a query lies outside the scope of the ancient Indian spiritual corpus or lacks a clear scriptural foundation, humbly state that no direct reference exists and respond only if it aligns with the spirit of those teachings.
+"""
+
+# - Your answers should specially focus on the Iskcon teachings of Chaitanya Mahaprabhu and srila prabhupada
---
+++ main.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
+import uvicorn
+import asyncio
+
+from fastapi.middleware.cors import CORSMiddleware
+
+from google import genai
+from google.genai import types
+import time
+
+from PROMPTS import system_prompts
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+# Gemeni Cliet
+client = genai.Client(api_key=gemini_key)
+
+# Create chat
+chat = client.chats.create(
+        model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompts,
+            temperature=0.3
+        ),
+)
+
+
+from pydantic import BaseModel
+
+# Define a request schema
+class ChatRequest(BaseModel):
+    query: str
+
+# FASTAPI
+app = FastAPI(title= "VEDA-PUTRA spiritual AI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://127.0.0.1:5500"] if serving from local server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+def lander():
+    return {"msg": "Welcome to Veda-putra"}
+
+@app.post('/chat')
+async def chat_with_ai(request: ChatRequest):
+    user_prompt = request.query
+    # user_prompt = body.get("query", "")
+    
+    if not user_prompt:
+        return {"error": "Query cannot be empty"}
+    
+    async def stream_response():
+        response = chat.send_message_stream(user_prompt)
+        yield "🟢 Assistant: "
+        for chunk in response:
+            try:
+                if chunk.text:
+                    yield chunk.text
+                    await asyncio.sleep(0.1)
+            except Exception as e:
+                yield f"\nError : {e}\n"
+        
+    return StreamingResponse(stream_response(), media_type="text/plain")
+
+                    
+if __name__ == '__main__':
+    uvicorn.run(app)
